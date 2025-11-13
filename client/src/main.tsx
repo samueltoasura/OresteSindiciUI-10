1f368582260091f0a0359f2a9441c5376ac4e305
samueltoasura <samuel.toasura@orestesindici.edu.co>
2025-11-13 05:46:32 +0000

    cambios

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.css"; // 👈 Importa aquí tu Tailwind
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
